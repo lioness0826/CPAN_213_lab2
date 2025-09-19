a64cb9860753ea7b0f1ba3db32229ac220f022b5
Kiwi <chenyujie217@163.com>
2025-09-19 16:43:43 -0400

    Initial commit for CPAN213 Lab2

---
+++ src/services/config.js
@@ -0,0 +1,8 @@
+import Config from 'react-native-config';
+export const AppConfig = {
+  apiBaseUrl: Config.API_BASE_URL,
+  appName: Config.APP_NAME,
+  debugMode: Config.DEBUG_MODE === 'true',
+  version: Config.VERSION,
+};
+export default AppConfig;
